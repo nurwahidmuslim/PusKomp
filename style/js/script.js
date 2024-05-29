7984196a0daeb1b03fb031674f48ffca562861b5
nurwahidmuslim <nurwahidmuslim32@gmail.com>
2024-05-29 13:11:20 +0700

    update denda

--- style/js/script.js
+++ style/js/script.js
@@ -1,15 +1,14 @@
 // Logic pengisian input peminjaman buku scr otomatis
 function setReturnDate() {
-            let borrowDate = new Date(document.getElementById("tgl_peminjaman").value);
-            let returnDate = new Date(borrowDate);
-            returnDate.setDate(borrowDate.getDate() + 7); // Menambahkan 7 hari
+  let borrowDate = new Date(document.getElementById("tgl_peminjaman").value);
+  let returnDate = new Date(borrowDate);
+  returnDate.setDate(borrowDate.getDate() + 7); // Menambahkan 7 hari
 
-            // Format tanggal pengembalian ke format yyyy-mm-dd
-            let formattedDate = returnDate.toISOString().split('T')[0];
-            
-            document.getElementById("tgl_pengembalian").value = formattedDate;
+  // Format tanggal pengembalian ke format yyyy-mm-dd
+  let formattedDate = returnDate.toISOString().split('T')[0];
+  
+  document.getElementById("tgl_pengembalian").value = formattedDate;
 }
-
 
 // Logic pengisian input denda dan keterlambatan pengembalian buku scr otomatis
 function hitungDenda() {
@@ -18,19 +17,20 @@ function hitungDenda() {
   const keterlambatan = document.getElementById('keterlambatan');
   const denda = document.getElementById('denda');
   
-  // Pastikan format tanggal adalah "YYYY-MM-DD"
-  let tglPengembalianFormatted = tglPengembalian.toISOString().split('T')[0];
-  let bukuKembaliFormatted = bukuKembali.toISOString().split('T')[0];
-  
   if (bukuKembali > tglPengembalian) {
-    keterlambatan.value = 'YA';
-    denda.value = 10000
+      keterlambatan.value = 'YA';
+
+      // Hitung jumlah hari keterlambatan
+      const timeDifference = bukuKembali - tglPengembalian;
+      const daysLate = Math.ceil(timeDifference / (1000 * 3600 * 24));
+
+      // Hitung denda berdasarkan jumlah hari keterlambatan
+      denda.value = daysLate * 5000;
   } else {
-    keterlambatan.value = 'Tidak';
-    denda.value = 0;
+      keterlambatan.value = 'Tidak';
+      denda.value = 0;
   }
 }
-
 
 // Panggil fungsi hitungDenda saat halaman dimuat
 window.onload = function() {
